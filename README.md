33fe2b1a085a1502f2153973c71c0725ddb5dad4
Sabri Eyuboglu <eyuboglu@stanford.edu>
2022-06-16 15:08:02 -0700

    Add quickstartf

--- README.md
+++ README.md
@@ -29,7 +29,7 @@ A longitudinal database of ML API predictions.
 History of APIs (HAPI) is a large-scale, longitudinal database of commercial ML API predictions. It contains 1.7 million predictions spanning APIs from Amazon, Google, IBM, Microsoft. The database include diverse machine learning tasks including image tagging, speech recognition and text mining from 2020 to 2022.
 
 
-## 💾 Downloading
+## 💾  Downloading
 The database is stored in a GCP bucket named [`hapi-data`](https://console.cloud.google.com/storage/browser/hapi-data). All model predictions are stored in [`hapi.tar.gz`](https://storage.googleapis.com/hapi-data/hapi.tar.gz) (Compressed size: `205.3MB`, Full size: `1.2GB`). 
     
 From the command line, you can download and extract the predictions with: 
@@ -43,13 +43,42 @@ We provide a lightweight python package for getting started with HAPI.
 pip install hapi @ git+https://github.com/lchen001/hapi@main
 ```
 
-Using a Jupyter notebook or some other interactive environment, you can import the library  
-and explore the data-centric problems in the benchmark:
+Import the library and specify the path to downloaded predictions (as described [above](#💾-downloading)). 
 
 ```python
 import hapi
-hapi.list()
+hapi.config.data_dir = "/path/to/data
 ```
+
+We can list the available APIs, datasets, and tasks with `hapi.list()`. This returns a [Pandas DataFrame](https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.html) with columns `task, dataset, api, date, path, cost_per_10k`. 
+```python
+df = hapi.list()
+```
+
+To load the predictions into memory we use `hapi.get_predictions()`. The keyword arguments allow us to load predictions for a subset of tasks, datasets, apis and/or dates. 
+```python
+predictions =
+hapi.get_predictions(task="mic", dataset="pascal", api=["google_mic", "ibm_mic"])
+```
+
+By default, the predictions are returned as a dictionary mapping from `"{task}/{dataset}/{api}/{date}"` to lists of dictionaries, each with keys `"id"`, `"prediction"`, `"confidence"`. For example,
+```python
+{
+    "mic/pascal/google_mic/20-10-28": [
+        {
+            'image_name': '2011_000494.jpg',
+            'confidence': 0.9798267782,
+            'example_id': '2011_000494',
+            'predicted_label': ['bird', 'bird']
+        },
+        ...
+    ],
+    "mic/pascal/microsoft_mic/20-10-28": [...],
+    ...
+}
+```
+
+
 
 
 ## 📚 Docs
---
+++ hapi/__init__.py
@@ -0,0 +1,88 @@
+from dataclasses import dataclass
+
+
+from dataclasses import dataclass
+import json
+import os
+from typing import List, Union
+from tqdm import tqdm
+
+
+@dataclass
+class HAPIConfig:
+    def __init__(self, data_dir: str, *args, **kwargs):
+        self._data_dir = data_dir
+        super().__init__(*args, **kwargs)
+
+    @property
+    def data_dir(self):
+        if self._data_dir is None or not os.path.exists(self._data_dir):
+            raise ValueError(
+                "Set `data_dir` in the hapi config to point to the directory where "
+                "you've downloaded hapi `hapi.config.data_dir = /path/to/data`."
+            )
+        return self._data_dir
+
+    @data_dir.setter
+    def data_dir(self, data_dir: str):
+        if not os.path.exists(os.path.join(data_dir, "meta.csv")):
+            raise ValueError(
+                "Trying to set `data_dir` to a directory that doesn't "
+                "contain `meta.csv`. Are you sure this is the directory where you've "
+                "downloaded hapi?"
+            )
+        self._data_dir = data_dir
+        assert os.path.exists(self._data_dir)
+
+
+config = HAPIConfig(data_dir=os.environ.get("HAPI_DATA_DIR", None))
+
+
+def download():
+    pass
+
+
+def list():
+    import pandas as pd
+
+    df = pd.read_csv(os.path.join(config.data_dir, "meta.csv"))
+    return df
+
+
+def get_predictions(
+    task: Union[str, List[str]] = None,
+    dataset: Union[str, List[str]] = None,
+    api: Union[str, List[str]] = None,
+    date: Union[str, List[str]] = None,
+):
+    df = list()
+    if task is not None:
+        if isinstance(task, str):
+            df = df[df["task"] == task]
+        else:
+            df = df[df["task"].isin(task)]
+
+    if dataset is not None:
+        if isinstance(dataset, str):
+            df = df[df["dataset"] == dataset]
+        else:
+            df = df[df["dataset"].isin(dataset)]
+
+    if api is not None:
+        if isinstance(api, str):
+            df = df[df["api"] == api]
+        else:
+            df = df[df["api"].isin(api)]
+
+    if date is not None:
+        if isinstance(date, str):
+            df = df[df["date"] == date]
+        else:
+            df = df[df["date"].isin(date)]
+
+    preds = {}
+    for path in tqdm(df["path"]):
+        preds[os.path.splitext(path)[0]] = json.load(
+            open(os.path.join(config.data_dir, path))
+        )
+    return preds
